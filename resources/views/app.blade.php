05df4c99758c656690f1518d78e2aa0d94b822d2
Mikey Rivera <mikeyriver2@gmail.com>
2019-09-28 14:24:12 +0800

    gulp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="/css/app.css" rel="stylesheet" type="text/cs">
-        <title>Pediatrix</title>
+        <title>SparkPledge</title>
     </head>
     <body>
         <div id="pediatrix">
